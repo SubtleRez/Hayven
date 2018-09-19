140bfa53b9285325e3afc6782b70504c403c766e
SubtleRez <jquancollier@hotmail.com>
2018-09-19 18:33:10 -0400

    Added Vector Class

---
+++ Hayven/Hayven/Vec3.cpp
@@ -0,0 +1,9 @@
+#include "Vec3.h"
+
+Vec3::Vec3(float x_in, float y_in, float z_in)
+	:
+	x(x_in),
+	y(y_in),
+	z(z_in)
+{
+}
---
+++ Hayven/Hayven/Vec3.h
@@ -0,0 +1,9 @@
+#pragma once
+
+class Vec3
+{
+private:
+	float x, y, z;
+public:
+	Vec3(float x_in, float y_in, float z_in);
+};
