83604148afffb828a01758c2d35c4e447f32a9fa
SubtleRez <jquancollier@hotmail.com>
2018-09-19 20:30:25 -0400

    Added basic math functions to Vec3

--- Hayven/Hayven/Vec3.h
+++ Hayven/Hayven/Vec3.h
@@ -1,9 +1,13 @@
 #pragma once
 
-class Vec3
-{
+class Vec3 {
 private:
 	float x, y, z;
 public:
 	Vec3(float x_in, float y_in, float z_in);
+
+	// Math Functions
+	Vec3 add(Vec3 a, Vec3 b);
+	Vec3 subtract(Vec3 a, Vec3 b);
+	Vec3 scale(Vec3 a, float c);
 };
